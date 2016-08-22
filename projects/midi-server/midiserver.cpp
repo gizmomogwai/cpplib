853dfa1efdfc4db81483d0dcede79a2296a531b2
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-08-22 14:32:53 +0200

    added test version of midi-server

---
+++ projects/midi-server/midiserver.cpp
@@ -0,0 +1,37 @@
+#include <RtMidi.h>
+#include <cstdlib>
+#include <iostream>
+
+void mycallback(double deltatime,
+                std::vector<unsigned char> *message,
+                void *userData) {
+  unsigned int nBytes = message->size();
+  for (unsigned int i = 0; i < nBytes; i++) {
+    std::cout << "Byte " << i << " = " << (int)message->at(i) << ", ";
+  }
+
+  if (nBytes > 0) {
+    std::cout << "stamp = " << deltatime << std::endl;
+  }
+}
+
+int main() {
+  RtMidiIn midiin;
+
+  unsigned int nPorts = midiin.getPortCount();
+  if (nPorts == 0) {
+    std::cout << "No ports available!\n";
+    return 0;
+  }
+
+  midiin.openPort(0);
+  int i=1;
+  midiin.setCallback(&mycallback, &i);
+  midiin.ignoreTypes(false, false, false);
+  std::cout << "\nReading MIDI input ... press <enter> to quit.\n";
+
+  char input;
+  std::cin.get(input);
+
+  return 0;
+}
---
+++ projects/midi-server/project.rb
@@ -0,0 +1,6 @@
+cxx_configuration do
+  exe "midi-server",
+    :sources => FileList['**/*.cpp'],
+    :includes => ['include'],
+    :dependencies => [BinaryLibrary.new('rtmidi')]
+end
